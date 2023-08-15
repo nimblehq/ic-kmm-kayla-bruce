17aa9332c72b08b8b4fff039521c4ffc125b1af3
Tam Nguyen Minh <nmint8m@gmail.com>
2023-08-15 11:12:23 +0700

    [#2] Uncomment BuildKonfig.example.kt

--- buildSrc/src/main/kotlin/appPackage/BuildKonfig.example.kt
+++ buildSrc/src/main/kotlin/appPackage/BuildKonfig.example.kt
@@ -1,9 +1,9 @@
-//object BuildKonfig {
-//    const val CLIENT_ID_PRODUCTION = ""
-//    const val CLIENT_SECRET_PRODUCTION = ""
-//    const val BASE_URL_PRODUCTION = ""
-//
-//    const val CLIENT_ID_STAGING = ""
-//    const val CLIENT_SECRET_STAGING = ""
-//    const val BASE_URL_STAGING = ""
-//}
+object BuildKonfig {
+    const val CLIENT_ID_PRODUCTION = ""
+    const val CLIENT_SECRET_PRODUCTION = ""
+    const val BASE_URL_PRODUCTION = ""
+
+    const val CLIENT_ID_STAGING = ""
+    const val CLIENT_SECRET_STAGING = ""
+    const val BASE_URL_STAGING = ""
+}
