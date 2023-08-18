54d054bc03b102ae8511ebf0045a3be13fb48290
Bruce - Thien Nguyen <thien@nimblehq.co>
2023-08-18 15:06:41 +0700

    [#65] Fix detekt

--- android/src/main/java/co/nimblehq/kaylabruce/kmmic/android/presentation/modules/home/HomeScreen.kt
+++ android/src/main/java/co/nimblehq/kaylabruce/kmmic/android/presentation/modules/home/HomeScreen.kt
@@ -8,8 +8,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import co.nimblehq.kaylabruce.kmmic.android.constants.Dimens
-import co.nimblehq.kaylabruce.kmmic.android.presentation.modules.home.home_header.HomeHeaderView
-import co.nimblehq.kaylabruce.kmmic.android.presentation.ui_models.HomeHeaderUiModel
+import co.nimblehq.kaylabruce.kmmic.android.presentation.modules.home.header.HomeHeaderView
+import co.nimblehq.kaylabruce.kmmic.android.presentation.uimodel.HomeHeaderUiModel
 
 @Composable
 fun HomeScreen() {
